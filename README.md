dc01b26e87797f83da45f0a330ded092ab5b8809
zoe <201241605@qq.com>
2025-01-06 19:31:08 +0800

    提交

---
+++ README.md
@@ -0,0 +1,16 @@
+# 苏米可-Miko の 歌单站
+
+
+
+修改自项目:
+
+[song-list-of-nanakaie](https://github.com/alan314m/song-list-of-nanakaie)
+
+[vup-song-list](https://github.com/Akegarasu/vup-song-list)
+
+[雨纪ameki的歌单](https://www.ameki.online)
+
+感谢以上项目的开源贡献。
+修改了若干处使项目更低门槛可以部署、编辑、发布。
+
+本项目遵守 MIT License
---
+++ config/constants.js
@@ -0,0 +1,50 @@
+let config = {
+    Name: "苏米可-Miko", // 主页名字
+  
+    BiliLiveRoomID: "", // 直播间id
+  
+    NetEaseMusicId: "", // 网易云音乐id
+    QQMusicId: "", // QQ音乐id
+    Footer: "Github",
+  
+    Cursor: false, // 使用自定义光标图片
+  
+    LanguageCategories: ["英语", "日语", "韩语"], // 语言分类
+    RemarkCategories: ["说唱", "戏腔", "儿歌"], // 标签分类
+  
+    BannerTitle: "米可-Miko！", // banner 标题
+  
+    BannerContent: ["喜欢米可，就请多多支持叭"
+    ],
+  
+    // 自定义按钮 （可以复制生成更多）
+    CustomButtons: [
+      {
+        link: "https://www.popibox.cn/u/8mayfv4c",
+		name: "匿名提问箱",
+        image: "/assets/icon/pome.png",
+        },
+
+
+      {
+        link: "https://v.douyin.com/iy5gVgv8/",
+        name: "抖音主页",
+        image: "/assets/icon/TikTok.png",
+        },
+      // {
+      //   link: "https://weibo.com/",
+      //   name: "微博",
+      //   image: "/assets/icon/weibo.png",
+      // },
+      /*{
+        link: "https://twitter.com",
+        name: "Twitter",
+        image: "/assets/icon/th.png",
+      },*/
+    ],
+};
+
+
+module.exports = {
+    config
+}
